f0e2ee69cd2f1ba798b3f5c157f705801e4af919
bility <292340644@qq.com>
2025-03-03 12:54:16 +0800

    1

---
+++ MorsCodeGame/Assets/RandItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RandItem : MonoBehaviour
+{
+    public TextMeshPro TextNo;
+    public TextMeshPro TextUserID;
+    public TextMeshPro TextCount;
+    public TextMeshPro Score;
+}
