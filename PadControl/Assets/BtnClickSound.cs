34e8eb515c37dc02e1af52096f33788d96c91e9f
bility <292340644@qq.com>
2025-04-09 17:15:05 +0800

    1

---
+++ PadControl/Assets/BtnClickSound.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnClickSound : MonoBehaviour
+{
+    public AudioClip clickSound; // 点击音效
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // 获取 AudioSource 组件
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+
+        // 设置音效
+        audioSource.clip = clickSound;
+
+        // 遍历场景中所有按钮
+        Button[] buttons = FindObjectsOfType<Button>();
+        foreach (Button button in buttons)
+        {
+            // 为按钮添加点击事件
+            button.onClick.AddListener(() => PlayClickSound());
+        }
+    }
+
+    void PlayClickSound()
+    {
+        // 播放音效
+        audioSource.Play();
+    }
+}
