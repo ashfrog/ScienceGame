be5228ea4af84fc3054fdd5c821dfc66c1cc698a
bility <292340644@qq.com>
2025-11-15 20:40:38 +0800

    slicer

--- XinTongYuan_SatelliteCar/Assets/SliderClickListener.cs
+++ XinTongYuan_SatelliteCar/Assets/SliderClickListener.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -11,10 +12,23 @@ public class SliderClickListener : MonoBehaviour, IPointerDownHandler
     [SerializeField]
     Slider slider;
 
-    public Action<float> SlicerClicked;
+    public UnityAction<float> SlicerClicked;
+
+    private void Start()
+    {
+        slider.onValueChanged.AddListener(OnSliderValueChanged);
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("点击slider 重置按钮的选中状态");
+        // 你的处理逻辑
+        groupManager.SetUnselected();
+        Debug.Log(slider.value);
+        SlicerClicked?.Invoke(slider.value);
+    }
+
+    public void OnSliderValueChanged(float value)
+    {
         // 你的处理逻辑
         groupManager.SetUnselected();
         Debug.Log(slider.value);
